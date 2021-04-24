e06935ec6b09d67e15212a6273c2b2c9b0ec2e13
Adam Vrána <adamvran@seznam.cz>
2021-04-24 21:58:27 +0200

    Linear complexity -> logaritmic complexity

--- Graph.cpp
+++ Graph.cpp
@@ -1,25 +1,68 @@
-#include <map>
-#include <vector>
-using namespace std;
-class Graph
+#include "Graph.h"
+#include <iostream>
+#include <queue>
+
+void Graph::createVertex(int value)
 {
-private:
-    class Vertex
+    vertexMap[value] = new Vertex(currentId, value);
+    currentId++;
+}
+
+void Graph::createUnorientedEdge(int value1, int value2)
+{
+    // Linear complexity -> SOLVE!!!
+    auto it = vertexMap.find(value1); //find má logaritmickou složitost
+    auto it2 = vertexMap.find(value2);
+
+    if (it != vertexMap.end() && it2 != vertexMap.end())
     {
-    public:
-        int id;
-        int value;
-        int color = 0;
+        Vertex* vertex1 = it->second;
+        Vertex* vertex2 = it2->second;
 
-        vector<Vertex*> neighbors;
+        vertex1->neighbors.push_back(vertex2);
+        vertex2->neighbors.push_back(vertex1);
+    }
 
-        Vertex(int id, int value):id(id), value(value){}
-    };
-    map<int, Vertex*> vertexMap;
-    int currentId = 0;
-public:
-    void createVertex(int value);
-    void createUnorientedEdge(int value1, int value2);
-    void print();
-    int breadthFirstSearch(int value);
-};
+}
+
+void Graph::print()
+{
+    for(auto mapElement : vertexMap)
+    {
+        Vertex* currentVertex = mapElement.second;
+
+        if (!currentVertex->neighbors.empty())
+            for(auto neightbor : currentVertex->neighbors)
+                cout << currentVertex->value << " -> " << neightbor->value <<endl;
+
+        else cout << currentVertex->value << endl;
+    }
+}
+
+int Graph::breadthFirstSearch(int value)
+{
+    Vertex* currentVertex = vertexMap.at(0);
+    queue<Vertex*> que;
+
+    que.push(currentVertex);
+    //currentVertex->color = 1;
+    while (!que.empty())
+    {
+        currentVertex = que.front();
+        que.pop();
+
+        currentVertex->color = 2;
+        cout << currentVertex->value << endl;
+
+        if (currentVertex->value == value)
+            return currentVertex->id;
+
+        for (auto neightbor : currentVertex->neighbors)
+            if (neightbor->color == 0)
+            {
+                que.push(neightbor);
+                neightbor->color = 1;
+            }
+    }
+    return 0;
+}
